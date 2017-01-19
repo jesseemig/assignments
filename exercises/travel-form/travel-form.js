56d4fd36e31bb212b36ec8cdbba3ae0ee450b26b
jesse emig <jesseemig@jesses-MacBook-Pro.local>
2017-01-18 20:01:08 -0700

    1-18-17

---
+++ exercises/travel-form/travel-form.js
@@ -0,0 +1,22 @@
+document.getElementById("btn").addEventListener("click", function myFunction() {
+   var fstNm = document.getElementsByName("firstName");
+    function firNam() {
+        alert(fstNm.value)
+    }
+    alert(fstNm);
+    alert("First name: \n" + "Last name: \n" + "Age: \n" + "Gender: \n" + "Locataion: \n" + "Dietary restrictions: ");
+    })
+ 
+
+
+// document.getElementById("perAge")addEventListener("click", function)
+// document.getElementById("male")addEventListener("click", function)
+// document.getElementById("female")addEventListener("click", function)
+// document.getElementById("utah")addEventListener("click", function)
+// document.getElementById("ny")addEventListener("click", function)
+// document.getElementById("alaska")addEventListener("click", function)
+// document.getElementById("veg")addEventListener("click", function)
+// document.getElementById("vegan")addEventListener("click", function)
+// document.getElementById("kosher")addEventListener("click", function)
+// document.getElementById("btn")addEventListener("click", function)
+
