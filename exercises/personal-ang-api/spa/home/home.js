e6e854cb1804ff618a7af891301341114a83e8c9
Jesse Emig <sam@example.com>
2017-02-12 21:31:17 -0700

    Personal ang just needs to be styled

--- exercises/personal-ang-api/spa/home/home.js
+++ exercises/personal-ang-api/spa/home/home.js
@@ -1,6 +1,23 @@
 var home = angular.module("routingApp")
 
-home.controller("HomeController", ["$scope", function($scope) {
-   $scope.output = "test"
+home.controller("HomeController", ["$scope", "$http", function ($scope, $http) {
+   
+     var config = {
+        headers: {"X-Mashape-Key":
+                 "Yx3ZIrcwZimshJB0uwTc7hb0VcUWp1DclwljsnudTovyVpzPe0"
+                }
+    };
+
+     $scope.findPh = function (input) {
+
+     $http.get("https://cosmin-us-phone-number-lookup.p.mashape.com/get.php?phone=" + input, config).then(function (response) {
+         
+         console.log(response.data);
+         var info = response.data;
+         $scope.output = info;
+        })
+     };
+
+    
 }]);
  
