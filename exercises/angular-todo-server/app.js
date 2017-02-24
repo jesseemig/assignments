49c7afd11f81e853fc52eb178b14290a36ca8933
Jesse Emig <sam@example.com>
2017-02-23 18:44:12 -0700

    personal api strikethough and delete work

--- exercises/angular-todo-server/app.js
+++ exercises/angular-todo-server/app.js
@@ -7,11 +7,11 @@ app.controller("MainController", ["$scope", "$http", function ($scope, $http) {
     $scope.upload = function () {
         $scope.todos.push($scope.add)
         $scope.add = {};
-    }
+    };
     
 
-    $scope.remove = function (thing) {
-        $scope.add.splice($scope.todos.indexOf(thing), 1) ;
+    $scope.remove = function (index) {
+        $scope.todos.splice(index, 1);
     }
 
 
