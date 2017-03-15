a15515a89473a5253a80b373382c2c33d7abb28a
Jesse Emig <sam@example.com>
2017-03-15 17:13:15 -0600

    list displays, checkbox does strikethrough, button deletes- Done

--- exercises/angular-todo-server/public/app.js
+++ exercises/angular-todo-server/public/app.js
@@ -12,28 +12,11 @@ app.controller("MainController", ["$scope", "$http", function ($scope, $http) {
     };
     
     
-    $http.get("http://api.vschool.io/jesseemig/todo").then(function (response) {
-        console.log(response.data);
-        var blah = (response.data)
-        $scope.todos = blah;
-    });
-    
-    
-    $http.post("http://api.vschool.io/jesseemig/todo", function (req, res) {
-    console.log(req.body);
-    var newBounty = req.body;
-    newBounty.id = uuid.v4();
-    bounties.push(newBounty);
-    res.send(newBounty);
-});
- //   $scope.remove = function (index) {
- //       $scope.todos.splice(index, 1);
- //   }
 
+    $scope.remove = function (index) {
+        $scope.todos.splice(index, 1);
+    };
 
-
- //   };
-
-  }]);
+}]);
 
 
