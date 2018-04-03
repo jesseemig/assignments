ddfdde6540cdca6707c6e2b8b8e13bb8cf96b2e3
Jesse Emig <jesseemig@hotmail.com>
2018-04-03 15:59:25 -0600

    Added a basic calculator with Angular

--- exercises/practice/public/app.js
+++ exercises/practice/public/app.js
@@ -1,12 +1,57 @@
-document.getElementById("addBtnId").addEventListener("click", addTotal);
-function addTotal() {
-    var add1 = document.getElementById("addId1").value;
+//  document.getElementById("addBtnId").addEventListener("click", addTotal);
+// function addTotal() {
+    // var add1 = document.getElementById("addId1").value;
 
-    var add2 = document.getElementById("addId2").value;
+    // var add2 = document.getElementById("addId2").value;
     
-    var added = +add1 + +add2;
-    document.getElementById("addTotal").textContent= added;
+    // var added = +add1 + +add2;
+    // document.getElementById("addTotal").textContent= added;
     
-}
+// }
 
-addTotal();
+// addTotal();
+
+// ^^^Above would allow adding function^^^
+
+
+
+
+// BELOW works for subtracting
+// var app = angular.module("MyApp", [])
+
+// app.controller("MainController", ["$scope", "$http", function ($scope, $http) {
+    
+    
+    // $scope.subTotal = function () {
+        // return $scope.first - $scope.second;
+        
+    // };
+    
+// }]);
+
+
+
+angular.module('MyApp', [])
+    .controller('CalculatorController', function($scope) {
+    
+    
+        $scope.result = function() {
+            if ($scope.operator == '+ Add') {
+                return $scope.a + $scope.b;
+            }
+            if ($scope.operator == '- Subtract') {
+                return $scope.a - $scope.b;
+            }
+            if ($scope.operator == '* Multiply') {
+                return $scope.a * $scope.b;
+            }
+            if ($scope.operator == '/ Divide') {
+                return $scope.a / $scope.b;
+            }
+        };
+    });
+
+
+
+
+
